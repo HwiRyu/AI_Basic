01b57a65e701bcc72cbc827086398fdfed560be1
HwiRwu <winter1152@uos.ac.kr>
2024-08-03 22:19:52 +0900

    0.0

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 def ReLU(x):
@@ -42,7 +43,7 @@ def nthnrowmatrix(L_i, L_i_minus_1):
 
 
 class Neural_Net:
-    def __init__(self, layer_size):  # 클래스가 갖는 속성을 설정.
+    def __init__(self, layer_size):
         self.layer_number = len(layer_size)
         self.W = ['Weight']
         for i in range(self.layer_number - 1):
@@ -50,8 +51,7 @@ class Neural_Net:
         self.B = ['Bias']
         for i in range(self.layer_number - 1):
             self.B.append(0.01 * np.random.randn(layer_size[i + 1], 1))
-
-        self.h = 0.005  # step size
+        self.h = 0.001  # step size
 
     def forward_propagation(self, input):
         self.L = ['Unassigned'] * self.layer_number
@@ -60,38 +60,42 @@ class Neural_Net:
         self.L[1] = self.W[1] @ self.L[0] + self.B[1]
         for i in range(2, self.layer_number):
             self.L[i] = self.W[i] @ ReLU(self.L[i - 1]) + self.B[i]
-        return softmax(self.L[self.layer_number - 1])
+        return softmax(self.L[-1])
 
     def back_propagation(self, input, target):
         N = self.layer_number - 1
         Y = self.forward_propagation(input)
         T = np.array(target).reshape(-1, 1)
-        Loss = MSE(Y, T)
 
         self.dL = ['Unassigned'] * (N + 1)  # 사용되는 부분이 중복되므로 DP 사용
         self.dW = ['D_Weight'] * (N + 1)
         self.dB = ['D_Bias'] * (N + 1)
 
-        for i in range(1, N + 1):
-            self.dL[i] = mse_derivative(Y, T).T @ softmax_derivative(self.L[N])
+        self.dL[N] = mse_derivative(Y, T).T @ softmax_derivative(self.L[N])
+        for i in range(N - 1, 0, -1):
+            self.dL[i] = self.dL[i + 1] @ self.W[i + 1] @ np.diagflat(ReLU_Derivative(self.L[i]))  # DP사용
 
-            for j in range(N, i, -1):
-                self.dL[i] = self.dL[i] @ self.W[j] @ np.diagflat(ReLU_Derivative(self.L[j - 1]))
+        for i in range(1, N + 1):
             self.dW[i] = (self.dL[i] @ nthnrowmatrix(self.L[i], self.L[i - 1])).reshape(self.W[i].shape)
             self.dB[i] = self.dL[i]
 
         for i in range(1, N + 1):
             self.W[i] -= self.h * self.dW[i]
             self.B[i] -= self.h * self.dB[i].T
+        Loss = MSE(Y, T)
+        print(Loss)
 
-        return Loss
 
+strat = time.time()
 
-NN = Neural_Net([10, 64, 128, 64, 32, 3])  # 신경망 선언
+NN = Neural_Net([10, 32, 32, 64, 128, 128, 64, 64, 32, 3])  # 신경망 선언
 X = [1, 2, 3, 4, 1, 2, -1, 2, -1, 0]  # 들어갈 input 정보
 T = [0.1, 0.3, 0.6]
 print(NN.forward_propagation(X))
 for i in range(10000):
-    print(NN.back_propagation(X, T))
-
+    NN.back_propagation(X, T)
 print(NN.forward_propagation(X))
+
+end = time.time()
+
+print(end - strat)
